6eff8a525f542f6f12ae7a9c33162de3085d21af
DavidGV0 <146364778+DavidGV0@users.noreply.github.com>
2023-09-28 17:49:16 +0200

    Add files via upload

---
+++ teamversus/src/main/java/com/ejemplo/teamversus/TeamversusApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.teamversus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamversusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamversusApplication.class, args);
+	}
+
+}
---
+++ teamversus/src/test/java/com/ejemplo/teamversus/TeamversusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.teamversus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamversusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
