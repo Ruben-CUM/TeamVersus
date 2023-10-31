ccfe4923ca04b3941d997dfcb7750940fa9e6e84
DavidGV0 <davidgomezv4@gmail.com>
2023-10-31 18:57:54 +0100

    Version 31Oct 1.4

--- teamversus/src/test/java/com/example/data/TeamversusApplicationTests.java
+++ teamversus/src/test/java/com/example/data/TeamversusApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.data.model.Pokemon;
 import com.example.data.model.Teamversus;
 import com.example.data.repository.TeamversusRepository;
 
@@ -18,6 +22,20 @@ class TeamversusApplicationTests {
 		System.out.println("Crear objeto principal Teamversus");
 		Teamversus teamversus = new Teamversus();
 		teamversus = teamversusRepository.save(teamversus);
+		String[] pokemonArray = {
+			    "Pikachu Electric",
+			    "Charizard Fire Flying",
+			    "Bulbasaur Grass Poison",
+			    "Squirtle Water",
+			    "Jigglypuff Normal Fairy"
+			};
+		Pokemon p1 = new Pokemon("Pikachu", "ELÉCTRICO", null);
+		Pokemon p2 = new Pokemon("Charizard", "FUEGO", "VOLADOR");
+		
+		
+		List<Pokemon> listaPokemon = new ArrayList<Pokemon>();
+		listaPokemon.add(p1);
+		
 	}
 
 }
