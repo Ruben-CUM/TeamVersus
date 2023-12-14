5c1b749c36b86cb424ef9f8e1ebc9393fcb3321f
Ruben-CUM <rgonzaleqq@alumnos.unex.es>
2023-12-14 18:14:46 +0100

    indexarreglado
    El css no lo coge no se por que

--- teamversus/src/main/java/com/example/data/controller/IndexController.java
+++ teamversus/src/main/java/com/example/data/controller/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	 @GetMapping("/")
-	    public String home() {
-	        return "Index";
-	    }
-	 
-	 public IndexController() {
-		
-	 }
+	//@GetMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+
+	public IndexController() {
+
+	}
 }
