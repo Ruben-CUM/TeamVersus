36ec3298b96bd249e704c4d1676a676fc1d9e7e2
DavidGV0 <davidgomezv4@gmail.com>
2023-12-14 18:35:30 +0100

    Version14Dic

--- teamversus/src/main/java/com/example/data/controller/IndexController.java
+++ teamversus/src/main/java/com/example/data/controller/IndexController.java
@@ -1,11 +1,18 @@
 package com.example.data.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	
+	@GetMapping("/combate")
+	public String combatePage (Model model) {
+		return "combate";
+	}
 
+<<<<<<< Updated upstream
 	//@GetMapping("/")
 	public String home() {
 		return "redirect:/index";
@@ -13,5 +20,19 @@ public class IndexController {
 
 	public IndexController() {
 
+=======
+	@GetMapping("/")
+	public String index() {
+		System.out.println("\t Recojo la peticion. "
+				+ "Devuelvo la vista index; "
+				+ "index.html esta ubicado en Templates");
+		return "index";
+	}
+
+
+	public IndexController() {
+		// TODO Auto-generated constructor stub
+		System.out.println("\t Builder of " + this.getClass().getSimpleName());
+>>>>>>> Stashed changes
 	}
 }
